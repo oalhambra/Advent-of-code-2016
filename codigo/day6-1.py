b2c61c61be3176b9cc609e10f338659edbb8de41
oalhambra <o.alhambra@gmail.com>
2016-12-06 11:59:59 +0100

    day 6 completed

---
+++ codigo/day6-1.py
@@ -0,0 +1,28 @@
+
+
+f = open('day6-1.txt', 'r')
+data=f.read()
+
+mensaje=data.split("\n")
+mensaje.pop(mensaje.__len__()-1)
+print(mensaje)
+
+
+for i in range(0, mensaje[0].__len__()):
+    frecuencia=[0 for k in range(27)]
+    for letra in range(0,mensaje.__len__()):
+        # print(i)
+        # print(letra)
+        # print(letra[i])
+        pos=ord(mensaje[letra][i])
+        pos -=97
+        frecuencia[pos]+=1
+    # print(frecuencia)
+    letraComun=-1
+    countLetras=-1
+    for j in range(0,27):
+        if frecuencia[j]>countLetras:
+            letraComun=j
+            countLetras=frecuencia[j]
+    print(chr(letraComun+97))
+
